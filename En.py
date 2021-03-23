b8dbd9252a7cdd5c8a070432d22f5df364d0c44a
Wanderer47 <strannek47@gmail.com>
2021-03-23 11:48:03 +0300

    automatically conversion

--- En.py
+++ En.py
@@ -3,24 +3,26 @@ import pathlib
 import os
 from googletrans import Translator
 
-# take audio file
-print("Input audio name: ")
-audio_name = input() # определяю название файла 'Q.wav' ...
-audio_fail_Directory = pathlib.Path("Audio_Input_En")
-audio_In = speech_recog.AudioFile(f'{audio_fail_Directory}/{audio_name}')
-# convert audio to text
+# make object
 r = speech_recog.Recognizer()
-with audio_In as audio_fail:
-    audio = r.record(audio_fail)
-line = r.recognize_google(audio, language='en-US')
-# translate En text in Ru lenguage 
 translator = Translator()
-result = translator.translate(line, dest='ru', src='en')
+# take audio file
+audio_fail_Directory = pathlib.Path("Audio_Input_En")
+audio_list = os.listdir(audio_fail_Directory)
+
+for audio_name in audio_list:
+    audio_In = speech_recog.AudioFile(f'{audio_fail_Directory}/{audio_name}')
+# convert audio to text
+    with audio_In as audio_fail:
+        audio = r.record(audio_fail)
+    line = r.recognize_google(audio, language='en-US')
+# translate En text in Ru lenguage 
+    result = translator.translate(line, dest='ru', src='en')
 # writ result to file - translate to ru
-text_Out_En_ru = os.path.join(pathlib.Path("Text_Output_En_ru"), f'{audio_name}.txt')
-with open(text_Out_En_ru, 'w') as text:
-    text.write(result.text)
+    text_Out_En_ru = os.path.join(pathlib.Path("Text_Output_En_ru"), f'{audio_name}.txt')
+    with open(text_Out_En_ru, 'w') as text:
+        text.write(result.text)
 # writ line to file - en
-text_Out_En_en = os.path.join(pathlib.Path("Text_Output_En_en"), f'{audio_name}.txt')
-with open(text_Out_En_en, 'w') as text:
-    text.write(line)
+    text_Out_En_en = os.path.join(pathlib.Path("Text_Output_En_en"), f'{audio_name}.txt')
+    with open(text_Out_En_en, 'w') as text:
+        text.write(line)
--- Ru.py
+++ Ru.py
@@ -2,18 +2,20 @@ import speech_recognition as speech_recog
 import pathlib
 import os
 
-# take audio file
-print("Input audio name: ")
-audio_name = input() # определяю название файла 'Q.wav' ...
-audio_fail_Directory = pathlib.Path("Audio_Input_Ru")
-audio_In = speech_recog.AudioFile(f'{audio_fail_Directory}/{audio_name}')
-# convert audio to text
+# make object
 r = speech_recog.Recognizer()
-with audio_In as audio_fail:
-    audio = r.record(audio_fail)
+# take audio file
+audio_fail_Directory = pathlib.Path("Audio_Input_Ru")
+audio_list = os.listdir(audio_fail_Directory)
+
+for audio_name in audio_list:
+    audio_In = speech_recog.AudioFile(f'{audio_fail_Directory}/{audio_name}')
+# convert audio to text
+    with audio_In as audio_fail:
+        audio = r.record(audio_fail)
 #line = r.recognize_sphinx(audio, language='en-US')
-line = r.recognize_google(audio, language='ru-RU')
+    line = r.recognize_google(audio, language='ru-RU')
 # writ line to file
-text_Out = os.path.join(pathlib.Path("Text_Output_Ru"), f'{audio_name}.txt')
-with open(text_Out, 'w') as text:
-    text.write(line)
+    text_Out = os.path.join(pathlib.Path("Text_Output_Ru"), f'{audio_name}.txt')
+    with open(text_Out, 'w') as text:
+        text.write(line)
